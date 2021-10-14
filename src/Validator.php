f45d6ba3a16fe09358b641d8fcd77735745c1996
Nanda Firmansyah <nafima21@gmail.com>
2021-10-14 14:08:08 +0700

    add credit to validator module

--- src/Validator.php
+++ src/Validator.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the HAISTAR Core Integration.
+ *
+ * (c) Nanda Firmansyah <nafima21@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Haistar;
 
 class Validator
