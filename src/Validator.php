8a80a0c8e29dc2538bbc80e809ef39c4ed798c22
Nanda Firmansyah <nafima21@gmail.com>
2021-10-14 14:07:22 +0700

    add validator module

---
+++ src/Validator.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Haistar;
+
+class Validator
+{
+    static public function make(Array $params, Array $rules, Array $custom_message)
+    {  
+        $error_message = "";
+
+        foreach ($rules as $key => $value)
+        {
+            if(!array_key_exists($key, $params))
+            {
+                if (strpos($value, 'required') !== false) 
+                {
+                    if(isset($custom_message))
+                    {
+                        if(array_key_exists($key.".required", $custom_message))
+                        {
+                            $error_message .= $custom_message[$key.".required"] . ", ";
+                        }
+                        else
+                        {
+                            $error_message .= $key . " must be declare, ";
+                        }
+                    }
+                    else
+                    {
+                        $error_message .= $key . " must be declare, ";
+                    }
+                }
+            }
+            else
+            {
+                if(empty($params[$key]))
+                {
+                    if (strpos($value, 'required') !== false) 
+                    {
+                        if(isset($custom_message))
+                        {
+                            if(array_key_exists($key.".required", $custom_message))
+                            {
+                                $error_message .= $custom_message[$key.".required"] . ", ";
+                            }
+                            else
+                            {
+                                $error_message .= $key . " cannot be empty, ";
+                            }
+                        }
+                        else
+                        {
+                            $error_message .= $key . " cannot be empty, ";
+                        }                        
+                    }
+                }
+            }
+        }
+        
+        return $error_message;
+    }
+}
